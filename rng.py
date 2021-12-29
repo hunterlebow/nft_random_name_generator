9b24d0508668a1c2893e60397b914d814796d995
Hunter Lebow <hunterlebow@gmail.com>
2021-12-29 05:13:27 -0500

    Second commit

--- rng.py
+++ rng.py
@@ -1,9 +1,10 @@
 #Author: Hunter Lebow
 import random
+import csv
 #Adjust k value to determine number of random generations.  
 # An optimized k hyperparameter should not be greater than the length of the largest TXT file to avoid repeats.
 k = 20
-'''Open TXT Files'''
+
 with open('adj.txt') as f:
     adj = f.read().splitlines()
     rand_adj = random.choices(adj, k = k)
@@ -16,13 +17,16 @@ with open ('club.txt') as fff:
     clb = fff.read().splitlines()
     rand_clb = random.choices(clb, k = k)
 
+records = [f'{a} {b} {c} Club' for (a, b, c) in zip(rand_adj, rand_anmls, rand_clb)]
+for i in range(len(records)):
+    records[i] = records[i].title()
+master = [row for row in enumerate(records)]   
 
-'''Random NFT Name Generator'''
-for (adj, anml, clb) in zip(rand_adj, rand_anmls, rand_clb):
-    nft_name = f'{adj} {anml} {clb} Club'
-    print(nft_name.title())
+with open('output.csv', 'w', encoding = 'UTF8', newline = '') as output:
+    writer = csv.writer(output)
+    writer.writerows(master)
+    output.close()
 
-'''Close TXT Files'''
 f.close()
 ff.close()
 fff.close()
